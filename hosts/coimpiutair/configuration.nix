f8b02675e6b54494b9ca25f5ada536646ac613a8
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2025-04-19 14:44:06 +1000

    kernel: replace with clang zen kernel with rust support

--- hosts/coimpiutair/configuration.nix
+++ hosts/coimpiutair/configuration.nix
@@ -19,7 +19,36 @@ args @ {
   ];
 
   # Use the latest linux kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackagesFor (
+    (pkgs.linux_zen.override {
+      stdenv = pkgs.overrideCC pkgs.llvmPackages_latest.stdenv (
+        # Tell our C compiler (Clang) to use LLVM bintools--normally GNU
+        # binutils are used even with Clang as the compiler.
+        pkgs.llvmPackages_latest.stdenv.cc.override {
+          bintools = pkgs.llvmPackages_latest.bintools;
+        }
+      );
+      # Tell Linux that we're compiling with Clang and LLVM.
+      extraMakeFlags = ["LLVM=1"];
+
+      # If you'd like to edit your kernel configuration, use
+      # `structuredExtraConfig`. For example, some options available to us
+      # when compiling with Clang and linking with LLD:
+      structuredExtraConfig = {
+        CC_OPTIMIZE_FOR_PERFORMANCE_O3 = lib.kernel.yes;
+
+        CFI_CLANG = lib.kernel.yes;
+        LTO_CLANG_FULL = lib.kernel.yes;
+
+        RUST = lib.kernel.yes;
+      };
+    }).overrideAttrs
+    # Work around another NixOS specific issue where builds with WERROR=y
+    # are stopped by a benign error. See reference 1 below for details.
+    # Technically, this fix is only necessary with WERROR=y but the issue
+    # still causes a warning on builds where WERROR is unset.
+    {env.NIX_CFLAGS_COMPILE = "-Wno-unused-command-line-argument";}
+  );
 
   # Use the systemd-boot EFI boot loader.
   boot.loader = {

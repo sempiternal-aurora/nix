ca666e39e28227a5f0380e764046f7aabdb87c51
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2025-04-25 12:10:59 +1000

    fw: add zramSwap

--- hosts/coimpiutair/configuration.nix
+++ hosts/coimpiutair/configuration.nix
@@ -135,6 +135,8 @@ args @ {
     support32Bit.enable = true;
   };
 
+  zramSwap.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;

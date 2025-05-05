1eacddda605a58b2d7fc176e2af076bdb38057b8
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2025-05-05 20:35:00 +0800

    nix: add optimise timer

--- hosts/coimpiutair/configuration.nix
+++ hosts/coimpiutair/configuration.nix
@@ -153,6 +153,10 @@ args @ {
       dates = "weekly";
       options = "--delete-older-than 10d";
     };
+    optimise = {
+      automatic = true;
+      dates = ["weekly"];
+    };
     settings = {
       auto-optimise-store = true;
     };

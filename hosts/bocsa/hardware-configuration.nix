b39db32aaa7c3605c6399039f46cacbc83059bc5
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2025-07-28 20:34:56 +1000

    bocsa: swap optiplex model

--- hosts/bocsa/hardware-configuration.nix
+++ hosts/bocsa/hardware-configuration.nix
@@ -14,10 +14,11 @@
   ];
 
   boot.initrd.availableKernelModules = [
+    "xhci_pci"
     "ehci_pci"
     "ahci"
-    "usbhid"
     "usb_storage"
+    "usbhid"
     "sd_mod"
     "sr_mod"
   ];
@@ -56,6 +57,7 @@
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s20u8.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;

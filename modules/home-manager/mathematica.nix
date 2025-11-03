4453f8c9ce5e86611eeb383feba39ed62086a069
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2025-11-04 00:04:33 +1100

    flake: update inputs

--- modules/home-manager/mathematica.nix
+++ modules/home-manager/mathematica.nix
@@ -12,8 +12,8 @@ in
     mine.mathematica.enable = lib.mkEnableOption "Enable Mathematica";
     mine.mathematica.version = lib.mkOption {
       type = lib.types.str;
-      default = "13.2.1";
-      example = "13.2.1";
+      default = "14.3.0";
+      example = "14.3.0";
     };
   };
 
@@ -21,15 +21,6 @@ in
     home.packages = [
       (pkgs.mathematica.override {
         version = cfg.version;
-        source = pkgs.requireFile {
-          name = "Mathematica_${cfg.version}_BNDL_LINUX.sh";
-          sha256 = "070ybhgskk3fw8c6fgqs4lq9252ds6585cqdd5as94hj55vjibmq";
-          message = ''
-            Your override for Mathematica includes a different src for the installer,
-            and it is missing.
-          '';
-          hashMode = "recursive";
-        };
       })
     ];
   };

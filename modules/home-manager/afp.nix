8e2d39877a377698487ee035b3548652ff5aab15
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2026-01-29 21:01:26 +0800

    isabelle: add afp

--- modules/home-manager/afp.nix
+++ modules/home-manager/afp.nix
@@ -1,16 +1,16 @@
 {
   stdenvNoCC,
   isabelle,
-  fetchhg,
+  fetchurl,
 }:
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "AFP";
-  version = "2025-1";
+  version = "2025-1-unstable-2026-01-23";
 
-  src = fetchhg {
-    url = "https://foss.heptapod.net/isa-afp/afp-2025-1";
-    rev = "0477baa439a72eea06f7c575ec05b6f59b1c6c5d";
+  src = fetchurl {
+    url = "https://foss.heptapod.net/isa-afp/afp-2025-1/-/archive/0477baa439a72eea06f7c575ec05b6f59b1c6c5d/afp-2025-1-0477baa439a72eea06f7c575ec05b6f59b1c6c5d.tar.gz";
+    hash = "sha256-xNix7r+3AAXDFpSgxvCNPB/MYJ/VmXIS4WfNhrLCjN4=";
   };
 
   buildPhase = ''

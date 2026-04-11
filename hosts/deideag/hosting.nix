895de700f3de026a830d0eba31fb26ccb743b5e6
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2026-04-11 17:05:39 +1000

    deideag: hopefully serve holonet

--- hosts/deideag/hosting.nix
+++ hosts/deideag/hosting.nix
@@ -13,9 +13,35 @@
   config = {
     services.caddy = {
       enable = true;
-      virtualHosts."holonet.myria.dev".extraConfig = ''
-        respond "Hello, world!"
-      '';
+
+      virtualHosts =
+        let
+          caddyConfig = url: {
+            extraConfig = ''
+              root * /var/www/${url}/public_html
+              file_server
+              php_fastcgi ${config.services.phpfpm.pools.holonet.socket}
+            '';
+          };
+        in
+        lib.attrsets.genAttrs [
+          "holonet.myria.dev"
+          "holonet.auroracod.ing"
+        ] caddyConfig;
+    };
+
+    services.phpfpm.pools.holonet = {
+      user = "php";
+      group = "php";
+      phpPackage = pkgs.php;
+      settings = {
+        "pm" = "dynamic";
+        "pm.max_children" = 75;
+        "pm.start_servers" = 10;
+        "pm.min_spare_servers" = 5;
+        "pm.max_spare_servers" = 20;
+        "pm.max_requests" = 500;
+      };
     };
 
     networking.firewall.allowedTCPPorts = [
